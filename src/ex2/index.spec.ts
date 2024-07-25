65618e219091a123cec7cfb011323f9a581f6d5a
ROBIN THOMAS <robin.19thomas@gmail.com>
2024-07-25 14:58:20 +0200

    exercice 2

--- src/ex2/index.spec.ts
+++ src/ex2/index.spec.ts
@@ -1,3 +1,31 @@
-describe("submitOrder", () => {
-  // écrire les tests ici
+import { submitOrder, Order } from './index';
+import { sendOrderEmail } from './lib/email';
+
+
+// Mock de la fonction sendOrderEmail()
+jest.mock('./lib/email', () => ({
+  sendOrderEmail: jest.fn(),
+}));
+
+describe('submitOrder', () => {
+  it('doit passer isSubmitted a true et appeller sendOrderEmail', () => {
+    const order: Order = {
+      id: '1',
+      isSubmitted: false,
+    };
+
+    const result = submitOrder(order);
+
+    expect(result.isSubmitted).toBe(true);
+    expect(sendOrderEmail).toHaveBeenCalledWith('1');
+  });
+
+  it('doit renvoyer une erreure si isSubmitted est déja égal à true', () => {
+    const order: Order = {
+      id: '2',
+      isSubmitted: true,
+    };
+
+    expect(() => submitOrder(order)).toThrow('La commande à dèja été passée.');
+  });
 });
--- src/ex2/index.ts
+++ src/ex2/index.ts
@@ -1,9 +1,18 @@
+import { sendOrderEmail } from './lib/email';
+
+
 export type Order = {
   id: string;
   isSubmitted: boolean;
 };
 
 export function submitOrder(order: Order): Order {
-  // ne pas oublier d'appeler la fonction `sendOrderEmail` (`src/ex2/lib/email.ts`)
+  if (order.isSubmitted) {
+    throw new Error('La commande à dèja été passée.');
+  }
+
+  order.isSubmitted = true;
+  sendOrderEmail(order.id);
+
   return order;
 }
